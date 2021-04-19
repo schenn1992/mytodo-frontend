ec03e895ac72cc90d854db8bcf27e64d6147ce5a
Steven Chen <schenn1992@gmail.com>
2021-04-18 22:33:27 -0400

    return original todo after canceling

--- src/components/EditTodo.js
+++ src/components/EditTodo.js
@@ -29,14 +29,14 @@ export default function EditTodo({todo}) {
           <div className="modal-content">
             <div className="modal-header">
               <h4 className="modal-title">Edit Todo</h4>
-              <button type="button" className="close" data-dismiss="modal">&times;</button>
+              <button type="button" className="close" data-dismiss="modal" onClick={() => setDescription(todo.description)}>&times;</button>
             </div>
             <div className="modal-body">
               <input type="text" className="form-control" value={ description } onChange={(event) => setDescription(event.target.value)} />
             </div>
             <div className="modal-footer">
               <button type="button" className="btn btn-primary" data-dismiss="modal" onClick= {(event) => updateDescription(event)}>Edit</button>
-              <button type="button" className="btn btn-danger" data-dismiss="modal">Close</button>
+              <button type="button" className="btn btn-danger" data-dismiss="modal" onClick={() => setDescription(todo.description)}>Close</button>
             </div>
           </div>
         </div>
